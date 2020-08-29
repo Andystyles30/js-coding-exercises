e8414a9852036ce3592cf1fb4e1a21325eda4a8d
Your Correct Name <andystyles30@gmail.com>
2020-08-29 15:24:07 +0100

    implement exercise001 reverseAllWords

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -43,12 +43,16 @@ function getMiddleCharacter(str) {
 
 function reverseWord(word) {
   if (word === undefined) throw new Error("word is required");
-  // Add your code here!
+  return word.split("").reverse().join("");
 }
 
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Add your code here!
+  let reversedWords = [];
+  for (word in words) {
+    reversedWords.push(reverseWord(words[word]));
+  }
+  return reversedWords;
 }
 
 function countLinuxUsers(users) {
