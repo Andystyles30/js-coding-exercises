3ed7d84f4f7ca8cc86e7398ac4b3ad92fa72bc1e
Your Correct Name <andystyles30@gmail.com>
2020-08-31 16:15:25 +0100

    implementation of exercise002 functions.

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,28 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  return person.city === "Manchester";
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  return Math.ceil(people / 40);
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  return arr.filter(function(sheep){ return sheep === "sheep"; }).length;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  let postcode = person.address.postCode;
+  let city = person.address.city;
+  return postcode.charAt(0) === "M" && city === "Manchester";
 }
 
 module.exports = {
