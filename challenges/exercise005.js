ba8364fb573d553110fe8e1f810d2847b7ede872
Your Correct Name <andystyles30@gmail.com>
2020-09-01 18:59:26 +0100

    start implementation of exercises 005

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -1,17 +1,35 @@
 const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  index = nums.indexOf(n);
+  if(index >= 0 && index < nums.length - 1)
+    return nextItem = nums[index + 1]
+  else
+    return null;
 };
 
 const count1sand0s = str => {
-  if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  var sum = 0;
+  for (num in str) {
+    sum += Number(str[num]);
+  }
+  return {
+    1 : sum,
+    0 : str.length - sum
+  };
 };
 
 const reverseNumber = n => {
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  return (
+    parseFloat(
+      n
+        .toString()
+        .split('')
+        .reverse()
+        .join('')
+    ) * Math.sign(n)
+  )  
 };
 
 const sumArrays = arrs => {
